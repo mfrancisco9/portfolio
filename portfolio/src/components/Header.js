c6d07ed45990f3a202ec4650842c9da81593109f
Michael Francisco <m.francisco9@gmail.com>
2021-06-24 11:20:51 -0500

    udpated boxes

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -12,7 +12,7 @@ export default function Header() {
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active">
+                    <li className="nav-item">
                         <a className="nav-link" href="#about">About</a>
                     </li>
                     <li className="nav-item">
--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -6,23 +6,31 @@ export default function Projects() {
         <div className="container-fluid main-box" id="projects">
             <div id="box-content">
 
-                <div id="main-project" className="row">
-                    main project goes here
-                    <div className="main-project-box col-12">img</div>
-                </div>
+                <a id="main-project" className="row" href="https://northwestern-stagecoach.herokuapp.com/">
+                    <span>Stagecoach</span>
+                    <div className="main-project-box col-12" id="stagecoach">
+                        <div className="project-description">Final project for Northwestern's coding bootcamp. A theoretical platform that allows small artists and venues to create profiles and book concerts.</div>
+                    </div>
+                </a>
 
                 <div className="row" id="other-projects-row">
-                    <div className="other-projects col-4">spotify
-                        <div className="sub-project">img and description</div>
-                    </div>
+                    <a className="other-projects col-4" href="https://github.com/loveliiivelaugh/spotify-community"><span>Spotify Playlist Project</span>
+                        <div className="sub-project" id="spotifyproject">
+                            <div className="project-description">Second project for Northwestern's coding bootcamp. Uses Spotify's API to give users greater control of music discovery and playlist generation.</div>
+                        </div>
+                    </a>
 
-                    <div className="other-projects col-4">moviego
-                        <div className="sub-project">img and description</div>
-                    </div>
+                    <a className="other-projects col-4" href="https://melanieuhrich.github.io/MovieGO/"><span>MovieGo</span>
+                        <div className="sub-project" id="moviego">
+                            <div className="project-description">First project for Northwestern's coding bootcamp. Utilizes basic HTML, CSS, and Javascript to create customized movie recommendations using the TMDb API.</div>
+                        </div>
+                    </a>
 
-                    <div className="other-projects col-4">some homework
-                        <div className="sub-project">img and description</div>
-                    </div>
+                    <a className="other-projects col-4" href="https://github.com/mfrancisco9?tab=repositories">Homework, smaller projects, works in progress
+                        <div className="sub-project">
+                            <div className="project-description">Additional work can be found on GitHub.</div>
+                        </div>
+                    </a>
 
                 </div>
 
