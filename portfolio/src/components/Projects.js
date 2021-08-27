fd85c0751d05482c4ec2af185603b3e6ad146773
Michael Francisco <m.francisco9@gmail.com>
2021-08-27 10:40:27 -0500

    hover images changed

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,5 +1,14 @@
 import React from "react";
 import "../css/projects.css";
+import { Link } from 'react-router-dom';
+
+// images
+import Flashcards from "../css/images/flashcards.jpg"
+import Stagecoach from "../css/images/stagecoach.jpg"
+import Misc from "../css/images/misc.jpg"
+import Moviego from "../css/images/moviego.jpg"
+import Spotify from "../css/images/spotify.jpg"
+import Coffee from "../css/images/coffee.jpg"
 
 export default function Projects({ changeCircle }) {
   return (
@@ -7,7 +16,14 @@ export default function Projects({ changeCircle }) {
       <div id="projects-box">
         <h3>Projects</h3>
 
-        <div className="projects-col" onMouseEnter={()=> changeCircle("https://i.imgur.com/0pQr5ha.jpg")}>
+        <div className="projects-col" onMouseEnter={()=> changeCircle(Flashcards)}>
+            <a href="https://arabic-flashcards-react.herokuapp.com/" className="project-title">Arabic Flashcards</a> | <a href="hhttps://github.com/mfrancisco9/arabic-flashcards">GitHub</a>
+            <div className="project-description">
+              Single-page application created to gain initial experience using the Arabic alphabet in the browser and SQL. Built with React, Express, and Sequelize. Deployed on Heroku with JawsDB. 
+            </div>
+          </div>
+
+        <div className="projects-col" onMouseEnter={()=> changeCircle(Stagecoach)}>
             <a href="https://northwestern-stagecoach.herokuapp.com/" className="project-title">Stagecoach</a> | <a href="https://github.com/mfrancisco9/StageCoach">GitHub</a>
             <div className="project-description">
               Final project for Northwestern's coding bootcamp. A theoretical
@@ -16,29 +32,29 @@ export default function Projects({ changeCircle }) {
             </div>
           </div>
 
-          <div className="projects-col" onMouseEnter={()=> changeCircle("https://i.imgur.com/mty71zZ.jpg")}>
+          <div className="projects-col" onMouseEnter={()=> changeCircle(Spotify)}>
             <a href="https://spotify-community.herokuapp.com/" className="project-title">Spotify Playlist Project</a> | <a href="https://github.com/loveliiivelaugh/spotify-community">GitHub</a>
             <div className="project-description">
             Second project for Northwestern's coding bootcamp. Uses Spotify's API to give users greater control of music discovery and playlist generation. Technologies used include SQL, OAuth, Express, and Spotify Dev Tools.
             </div>
           </div>
 
-          <div className="projects-col" onMouseEnter={()=> changeCircle("https://i.imgur.com/W5fd8Dq.jpg")}>
+          <div className="projects-col" onMouseEnter={()=> changeCircle(Moviego)}>
             <a href="https://melanieuhrich.github.io/MovieGO/" className="project-title">MovieGo</a> | <a href="https://github.com/mfrancisco9/MovieGO">GitHub</a>
             <div className="project-description">
             First project for Northwestern's coding bootcamp. Utilizes basic HTML, CSS, and Javascript to create customized movie recommendations using the TMDb API.
             </div>
           </div>
 
-          <div className="projects-col" onMouseEnter={()=> changeCircle("https://i.imgur.com/KSrceNz.jpg")}>
+          <div className="projects-col" onMouseEnter={()=> changeCircle(Coffee)}>
             <a href="https://mfrancisco9.github.io/coffee-dialer/" className="project-title">Coffee Dialer</a> | <a href="https://github.com/mfrancisco9/coffee-dialer">GitHub</a>
             <div className="project-description">
               A simple dashboard for coffee brewing created with React. Saves past entries in local storage and provides recommendations for a better cup.
             </div>
           </div>
 
-          <div className="projects-col" onMouseEnter={()=> changeCircle("https://i.imgur.com/KSrceNz.jpg")}>
-            <a href="https://mfrancisco9.github.io/coffee-dialer/" className="project-title">Odds and Ends</a> 
+          <div className="projects-col" onMouseEnter={()=> changeCircle(Misc)}>
+            <Link to="/misc" className="project-title">Odds and Ends</Link> 
             <div className="project-description">
               Various single-page works, created quickly with an emphasis on design.
             </div>
